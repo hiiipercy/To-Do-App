25242c609035686f839d244da2580ee4c4e61311
Eshaq Miah <129651038+hiiipercy@users.noreply.github.com>
2023-11-06 10:41:54 +0600

    Update text.js

--- text.js
+++ text.js
@@ -1,4 +1,4 @@
-const inputBox=document.getElementById("input-task");
+/*const inputBox=document.getElementById("input-task");
 const inputList=document.getElementById("item-list");
 
 function Additem(){
@@ -15,12 +15,71 @@ function Additem(){
     }
     
     inputBox.value="";
+    saveData();
 }
 
 inputList.addEventListener("click", function(item){
     if(item.target.tagName === "LI"){
         item.target.classList.toggle("checked");
-    }else if(item.target.tagName === "SPAN"){
+        saveData();
+    }
+    
+    else if(item.target.tagName === "SPAN"){
         item.target.parentElement.remove();
+        saveData();
+    }
+
+},false);
+
+function saveData(){
+    localStorage.setItem("Data", itemlist.innerHTML);
+}
+function showData(){
+    itemlist.innerHTML= localStorage.getItem("Data");
+}
+showData();*/
+const inputBox=document.getElementById("input-task");
+const inputList=document.getElementById("itemlist");
+
+function addItem (){
+    if(inputBox.value === ""){
+        alert("Add item first!");
+    }
+    else if(inputBox.value.length >25 ){
+        alert("Write less than 25 charecter");
+    }
+
+    else {
+        let li=document.createElement("LI");
+        li.innerHTML=inputBox.value;
+        inputList.appendChild(li);
+
+        let span=document.createElement("SPAN");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+        
+    }
+    inputBox.value="";
+    saveData();
+    
+}
+
+inputList.addEventListener("click", function(item){
+    if(item.target.tagName === "LI"){
+       item.target.classList.toggle("checked");
+       saveData();
+    }
+    
+    else if(item.target.tagName ==="SPAN"){
+        item.target.parentElement.remove();
+        saveData();
     }
 },false)
+
+function saveData(){
+    localStorage.setItem("Data", inputList.innerHTML);
+}
+function showData(){
+    inputList.innerHTML= localStorage.getItem("Data");
+}
+showData();
